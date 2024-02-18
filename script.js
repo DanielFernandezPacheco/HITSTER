b903c4b23dbd05f116d1dd2a78d149a237e134da
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2024-02-18 22:34:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const songId = parseInt(urlParams.get('id'));
-const audioPlayer = document.getElementById('audioPlayer');
 
 fetch('songs.json')
     .then(response => response.json())
@@ -9,9 +8,30 @@ fetch('songs.json')
         const song = data.find(song => song.id === songId);
 
         if (song) {
+
+            const audioPlayer = document.getElementById('audioPlayer');
             audioPlayer.src = song.url;
         } else {
             console.error('Canción no encontrada.');
         }
     })
     .catch(error => console.error('Error al cargar el JSON:', error));
+
+const imagen = document.getElementById('disco');
+
+audioPlayer.addEventListener('play', function () {
+    // Añadir la clase 'rotando' para iniciar la rotación
+    imagen.classList.add('rotando');
+});
+
+// Evento que se activa cuando el audio se pausa
+audioPlayer.addEventListener('pause', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
+
+// Evento que se activa cuando el audio termina de reproducirse
+audio.addEventListener('ended', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
