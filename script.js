ef0f6ac949c2d0f3697b95febb8635ed9a390c62
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2024-02-18 22:48:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,34 +20,19 @@ fetch('songs.json')
 
 const imagen = document.getElementById('disco');
 
-let rotacionActual = 0; // Variable para almacenar la rotación actual
+audioPlayer.addEventListener('play', function () {
+    // Añadir la clase 'rotando' para iniciar la rotación
+    imagen.classList.add('rotando');
+});
 
-    // Evento que se activa cuando el audioPlayer empieza a reproducirse
-    audioPlayer.addEventListener('play', function() {
-        // Recuperar la rotación actual de la imagen
-        const transform = window.getComputedStyle(imagen).getPropertyValue('transform');
-        // Extraer el ángulo de rotación actual
-        const match = transform.match(/matrix\((.*)\)/);
-        if (match && match[1]) {
-            const matrixValues = match[1].split(',');
-            rotacionActual = Math.round(Math.atan2(matrixValues[1], matrixValues[0]) * (180/Math.PI));
-        }
-        // Añadir la clase 'rotando' para iniciar la rotación
-        imagen.classList.add('rotando');
-    });
+// Evento que se activa cuando el audio se pausa
+audioPlayer.addEventListener('pause', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
 
-    // Evento que se activa cuando el audioPlayer se pausa
-    audioPlayer.addEventListener('pause', function() {
-        // Quitar la clase 'rotando' para detener la rotación
-        imagen.classList.remove('rotando');
-        // Aplicar la rotación actual como estilo en línea
-        imagen.style.transform = `rotate(${rotacionActual}deg)`;
-    });
-
-    // Evento que se activa cuando el audioPlayer termina de reproducirse
-    audioPlayer.addEventListener('ended', function() {
-        // Quitar la clase 'rotando' para detener la rotación
-        imagen.classList.remove('rotando');
-        // Restablecer la rotación actual
-        rotacionActual = 0;
-    });
+// Evento que se activa cuando el audio termina de reproducirse
+audio.addEventListener('ended', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
