39ab3a30a5e05d25d6773fb52fad8dd26e5dae38
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2024-02-19 00:51:08 +0100

    qr reader

--- script.js
+++ script.js
@@ -36,3 +36,33 @@ audio.addEventListener('ended', function () {
     // Quitar la clase 'rotando' para detener la rotación
     imagen.classList.remove('rotando');
 });
+
+// Configuración del lector de QR
+Quagga.init({
+    inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('#reader'),    // Selector del contenedor
+        constraints: {
+            width: 640,
+            height: 480,
+            facingMode: "environment" // Opcional: "environment" para la cámara trasera
+        },
+    },
+    decoder : {
+        readers : ["code_128_reader"] // Lista de tipos de códigos a leer (QR, code_128, etc.)
+    }
+}, function(err) {
+    if (err) {
+        console.error(err);
+        return
+    }
+    console.log("Lector de QR inicializado correctamente");
+    Quagga.start();
+});
+
+// Escucha el evento 'detected' para obtener el resultado del escaneo
+Quagga.onDetected(function(result) {
+    console.log("Resultado del escaneo:", result.codeResult.code);
+    // Aquí puedes hacer lo que quieras con el resultado, como redirigir a una página o mostrar información.
+});
