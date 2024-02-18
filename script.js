e2fba37f2cbf8915a0ce9fc830a9f9c4a4fc569e
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2024-02-18 22:19:00 +0100

    girar disco cuando se reproduce canción

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-        const urlParams = new URLSearchParams(window.location.search);
-        const songId = parseInt(urlParams.get('id'));
+const urlParams = new URLSearchParams(window.location.search);
+const songId = parseInt(urlParams.get('id'));
+const audioPlayer = document.getElementById('audioPlayer');
 
-        fetch('songs.json')
-            .then(response => response.json())
-            .then(data => {
-                // Buscar la canción con el ID correspondiente
-                const song = data.find(song => song.id === songId);
+fetch('songs.json')
+    .then(response => response.json())
+    .then(data => {
+        // Buscar la canción con el ID correspondiente
+        const song = data.find(song => song.id === songId);
 
-                if (song) {
-                    const audioPlayer = document.getElementById('audioPlayer');
-                    audioPlayer.src = song.url;
-                } else {
-                    console.error('Canción no encontrada.');
-                }
-            })
-            .catch(error => console.error('Error al cargar el JSON:', error));
+        if (song) {
+            audioPlayer.src = song.url;
+        } else {
+            console.error('Canción no encontrada.');
+        }
+    })
+    .catch(error => console.error('Error al cargar el JSON:', error));
+
+
+audio.addEventListener('play', function () {
+    // Añadir la clase 'rotando' para iniciar la rotación
+    imagen.classList.add('rotando');
+});
+
+// Evento que se activa cuando el audio se pausa
+audio.addEventListener('pause', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
+
+// Evento que se activa cuando el audio termina de reproducirse
+audio.addEventListener('ended', function () {
+    // Quitar la clase 'rotando' para detener la rotación
+    imagen.classList.remove('rotando');
+});
