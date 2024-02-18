1a6ef232f82ba6b46c2fb92ac856c25591baeb56
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2024-02-18 22:11:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
- // Función para obtener el valor del parámetro GET por nombre
-        // Obtener el ID de la canción desde el parámetro GET 'id'
         const urlParams = new URLSearchParams(window.location.search);
         const songId = parseInt(urlParams.get('id'));
 
-        // Cargar el JSON con los datos de las canciones
         fetch('songs.json')
             .then(response => response.json())
             .then(data => {
                 // Buscar la canción con el ID correspondiente
                 const song = data.find(song => song.id === songId);
 
-                // Mostrar la información de la canción
                 if (song) {
-                    console.log(songId);
-                    // Obtener el reproductor de audio
                     const audioPlayer = document.getElementById('audioPlayer');
                     audioPlayer.src = song.url;
                 } else {
